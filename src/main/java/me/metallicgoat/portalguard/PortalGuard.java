f72130d0ce41f46239eda4c849c176ef70df1047
MetallicGoat <the.great.metallic.goat@gmail.com>
2023-10-19 12:11:54 -0400

    Add Config

--- src/main/java/me/metallicgoat/portalguard/PortalGuard.java
+++ src/main/java/me/metallicgoat/portalguard/PortalGuard.java
@@ -1,5 +1,7 @@
 package me.metallicgoat.portalguard;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,24 +9,14 @@ import org.bukkit.event.entity.EntityPortalEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Arrays;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PortalGuard extends JavaPlugin implements Listener {
+	private final List<EntityType> blacklistedEntities = new ArrayList<>();
 
-	private final List<EntityType> blacklistedEntities = Arrays.asList(
-			EntityType.DROPPED_ITEM,
-			EntityType.BOAT,
-			EntityType.MINECART,
-			EntityType.MINECART_CHEST,
-			EntityType.MINECART_COMMAND,
-			EntityType.MINECART_FURNACE,
-			EntityType.MINECART_TNT,
-			EntityType.MINECART_HOPPER,
-			EntityType.MINECART_MOB_SPAWNER
-	);
-
-	public void onEnable(){
+	public void onEnable() {
 		final PluginDescriptionFile pdf = this.getDescription();
 
 		log(
@@ -36,6 +28,40 @@ public class PortalGuard extends JavaPlugin implements Listener {
 		);
 
 		getServer().getPluginManager().registerEvents(this, this);
+		loadConfig();
+	}
+
+	private void loadConfig() {
+		final File configFile = new File(getDataFolder(), "config.yml");
+
+		if (!configFile.exists()) {
+			configFile.getParentFile().mkdirs();
+			saveResource("config.yml", false);
+		}
+
+		final FileConfiguration config = new YamlConfiguration();
+
+		try {
+			config.load(configFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		final List<String> entityTypes = config.getStringList("Blacklisted-Portal-Entities");
+
+		if (entityTypes == null) {
+			getLogger().warning("Your PortalGuard config seems to be missing the 'Blacklisted-Portal-Entities' config!");
+			return;
+		}
+
+		for(String type : entityTypes){
+			try {
+				final EntityType entityType = EntityType.valueOf(type);
+				blacklistedEntities.add(entityType);
+			} catch (Exception e){
+				getLogger().warning("EntityType '" + type + "' does not exist!");
+			}
+		}
 	}
 
 	@EventHandler
