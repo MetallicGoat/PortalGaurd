325c70a6c891703d4fe6157cbeecfdb89cc78e7e
MetallicGoat <the.great.metallic.goat@gmail.com>
2023-10-18 22:05:07 -0400

    Block boats & minecarts

--- src/main/java/me/metallicgoat/portalguard/PortalGuard.java
+++ src/main/java/me/metallicgoat/portalguard/PortalGuard.java
@@ -7,7 +7,22 @@ import org.bukkit.event.entity.EntityPortalEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PortalGuard extends JavaPlugin implements Listener {
+
+	private final List<EntityType> blacklistedEntities = Arrays.asList(
+			EntityType.DROPPED_ITEM,
+			EntityType.BOAT,
+			EntityType.MINECART,
+			EntityType.MINECART_CHEST,
+			EntityType.MINECART_COMMAND,
+			EntityType.MINECART_FURNACE,
+			EntityType.MINECART_TNT,
+			EntityType.MINECART_HOPPER,
+			EntityType.MINECART_MOB_SPAWNER
+	);
 
 	public void onEnable(){
 		final PluginDescriptionFile pdf = this.getDescription();
@@ -25,7 +40,7 @@ public class PortalGuard extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void onPortalUse(EntityPortalEvent event) {
-		if (event.getEntityType() == EntityType.DROPPED_ITEM) {
+		if (blacklistedEntities.contains(event.getEntityType())) {
 			event.setCancelled(true);
 		}
 	}
